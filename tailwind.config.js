c9a23bc56a2533cc70ebc0543de5b2b472bc9b34
Moong <tmdals9154@gmail.com>
2023-09-06 02:28:08 +0900

    🎉 프로젝트 세팅
    이미지 최적화, 테일윈드 프레임 구성

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.jsx'],
   theme: {
+    screens: {
+      mobile: { min: "320px", max: "767px" },
+
+      tablet: { min: "768px", max: "1279px" },
+
+      desktop: { min: "1280px" },
+      contents: { max: '1280px' }
+    },
+
+
     extend: {
       fontFamily: {
         '이름' : ["ProximaNova-Semibold"],
         '이름2' : ["NotoSansKR-Medium"]
       },
+      colors: {
+        transparent: 'transparent',
+        primary: '#000',
+        secondary: '#fff',
+      },
+      backgroundImage: {},
     },
+    
   },
   plugins: [],
 }
