9737f412fc49335ac25d6ac9d9d0d89fc23c135a
lagom60 <lagom6061@gmail.com>
2023-09-12 00:57:29 +0900

    ➕ [Add] Footer 추가 #37
    App.jsx 에 Footer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Category from './views/Category';
 import Login from './components/Login';
 import Header from './layout/Header';
+import Detail from './views/Detail';
+import Footer from './layout/Footer';
 
 /**
  *  App은 가장 기본이 되는 함수
  * */
-
-import Detail from './views/Detail';
-import Footer from './layout/Footer';
 
 function App() {
 	return (
