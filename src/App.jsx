42b1ac296304fa68b05c711c263e8c9122e76a8e
lagom60 <lagom6061@gmail.com>
2023-09-15 00:50:18 +0900

    🔥 [Remove] 이미지 중복 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import Login from './components/Login';
 import Header from './layout/Header';
 import Detail from './views/Detail';
 import Footer from './layout/Footer';
-<<<<<<< HEAD
 import Mainpage from './views/Mainpage';
-=======
-// import Mainpage from './views/Mainpage';
->>>>>>> b0d5fa4c836f9c0731472ad4062280c5ac239376
 
 function App() {
 	return (
