b0d5fa4c836f9c0731472ad4062280c5ac239376
lagom60 <lagom6061@gmail.com>
2023-09-14 09:39:45 +0900

    💥 [Merge] develop merge

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
 			{/* <Header /> */}
 			{/* <Login /> */}
 			{/* <Mainpage /> */}
-			{/* <Category /> */}
-			<Detail/>
+			<Category />
+			{/* <Detail/> */}
 			<Footer />
 		</>
 );
