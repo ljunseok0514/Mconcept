5e7348b2f48905787856fed8da35233d19bb6f15
ljunseok0514 <dlwnstjr456@naver.com>
2023-09-15 12:06:31 +0900

    ➕ [Add]  회원가입 마크업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import Login from './views/Login';
 import Header from './layout/Header';
 import Detail from './views/Detail';
 import Footer from './layout/Footer';
+import Join from './views/Join';
 // import Mainpage from './views/Mainpage';
 
 function App() {
 	return (
 		<>
-			{/* <Header /> */}
+			<Header />
 			{/* <Login /> */}
 			{/* <Mainpage /> */}
-			<Category />
-			{/* <Detail/> */}
-			<Footer />
+
+			<Join />
+			{/* <Category /> */}
+			{/* <Detail /> */}
+			{/* <Footer /> */}
 		</>
 	);
 }
