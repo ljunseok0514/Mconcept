29afbf0506d5253e7eee3ee1f2ce67f32fc0b77a
lagom60 <lagom6061@gmail.com>
2023-09-18 08:55:11 +0900

    🎨 [Styling] 라벨 4개 분리 #55
    - 색상 분리
    - 각 라벨 이름 내보내기
    - category에서 조건부 렌더링

--- src/components/category/ProductLabel.jsx
+++ src/components/category/ProductLabel.jsx
@@ -1,36 +1,62 @@
 import React, { useState } from 'react';
 
-function Label({ isVisible }) {
+function Label({ isVisible, label }) {
   if (!isVisible) return null;
   
-  return <label className="sr-only">뉴시즌</label>;
+  return <label className="sr-only">{label}</label>;
 }
 
-function VisibilityButton({isVisible}) {
+function VisibilityButton({isVisible, label, backgroundColor, borderColor, color}) {
     return (
-        <label 
-          className="bg-tertiary p-[2px] px-1 text-center text-xs font-medium text-white" 
-          role="button"
-          aria-label={isVisible ? '뉴시즌' : 'hide'}
+        <button 
+          className="bg-tertiary mt-1 p-[2px] px-1 text-center text-xs font-medium" 
+          role="label"
+          aria-label={isVisible ? label : 'hide'}
+          style={{ backgroundColor, border:`1px solid ${borderColor}`, color }}
+          disabled
         >
-          {isVisible ? '뉴시즌' : 'hide'}
-        </label>
+          {isVisible ? label : 'hide'}
+        </button>
       );
-    }
+}
 
-function ProductLabel() {
-  const [isLabelVisible, setLabelVisible] = useState(true);
-
-  const handleClick = () => {
-    setLabelVisible(!isLabelVisible);
-  };
+export function NewSeasonLabel() {
+  const [isNewSeasonVisible] = useState(true);
 
   return (
-    <div className='mt-4'>
-      <Label isVisible={isLabelVisible} />
-      <VisibilityButton isVisible={isLabelVisible}/>
+    <div>
+      <Label isVisible={isNewSeasonVisible} label="뉴시즌" />
+      <VisibilityButton backgroundColor="#fa5500" color='white' isVisible={isNewSeasonVisible} label="뉴시즌"/>
     </div>
   );
 }
 
-export default ProductLabel;
+export function CelebrityLabel() {
+const [isCelebVisible ] = useState(true);
+
+return (
+<div>
+<Label isVisible={isCelebVisible} label="셀럽착용"/>
+<VisibilityButton backgroundColor="white" borderColor='grey' color='grey' isVisible={isCelebVisible} label="셀럽착용"/>
+</div>);
+}
+
+export function CouponLabel() {
+const [isCouponvisible] = useState(true);
+
+return (
+<div>
+<Label isVisible={isCouponvisible } label="쿠폰"/>
+<VisibilityButton backgroundColor="black" color='white' isVisible={isCouponvisible } label="쿠폰"/>
+</div>);
+}
+
+export function OnlyLabel() {
+const [isOnlyVisible] = useState(true);
+
+return (
+<div>
+<Label isVisible={isOnlyVisible } label="단독판매 "/>
+<VisibilityButton backgroundColor="white" borderColor='grey' color='grey' isVisible ={ isOnlyVisible } label="단독판매 "/>
+</div>);
+}
