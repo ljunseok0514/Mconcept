e70727ddf153d6ac417e9c73e50e453c75aef4d9
lagom60 <lagom6061@gmail.com>
2023-09-18 13:45:28 +0900

    ✨ [Feat] routers 설치 및 카테고리 적용 #86
    category router 경로 설정

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,20 +1,18 @@
 import {Route, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
-
-// 컴포넌트 경로
 import Header from '@/layout/Header';
 import Login from '@/pages/Login';
 import Mainpage from '@/pages/Mainpage';
 import Join from '@/pages/Join';
 import RootLayout from '@/layout/RootLayout';
-// 컴포넌트 경로
+import RootLayout from '@/pages/Category';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-		// 경로 설정 하는 부분
 		<Route path="/" element={<RootLayout />}>
 			<Route index element={<Mainpage />} />
 			<Route path="join" element={<Join />} />
 			<Route path="login" element={<Login />} />
+			<Route path="Category" element={<Category />} />
 		</Route>,
 	),
 );
