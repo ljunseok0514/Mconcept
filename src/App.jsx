1abca1e7c2bd44ab0c4600b88ff54fd683144fbc
lagom60 <lagom6061@gmail.com>
2023-09-20 07:35:31 +0900

    🔥 [Remove] 주석 제거

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import {RouterProvider} from 'react-router-dom';
 import router from './routes/routes';
 import AuthProvider from './contexts/Auth';
 import {Toaster} from 'react-hot-toast';
-// import Mainpage from './views/Mainpage';
 
 function App() {
 	return (
