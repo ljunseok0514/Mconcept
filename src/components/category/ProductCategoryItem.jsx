9e2909d31599e3565ef213febd562d04eb1891fc
lagom60 <lagom6061@gmail.com>
2023-09-21 16:58:16 +0900

    🏗️ [Structure] ProductCategoryItem 코드 구조 변경 #95

--- src/components/category/ProductCategoryItem.jsx
+++ src/components/category/ProductCategoryItem.jsx
@@ -3,68 +3,71 @@ import '../../styles/category.css';
 
 // 카테고리 및 아이템 데이터
 const categories = [
-	{
+	{	
+		id: '1',
 		title: '아우터',
 		items: ['VIEW ALL OUTER', '자켓', '점퍼', '패딩', '코트'],
 	},
 	{
+		id: '2',
 		title: '팬츠',
 		items: ['VIEW ALL PANTS', '슬랙스', '쇼츠', '치노', '스웻'],
 	},
 	{
+		id: '3',
 		title: '티셔츠',
 		items: ['VIEW ALL T-SHIRTS', '반팔', '스웻', '후드', '긴팔'],
 	},
 	{
+		id: '4',
 		title: '니트',
-		items: ['VIEW ALL NEAT', '반팔', '스웻', '후드', '긴팔'],
+		items: ['VIEW ALL NEAT', '풀오버', '가디건', '베스트', '터틀넥'],
 	},
 ];
 
 
 function ProductCategoryItem() {
-	const [activeItem, setActiveItem] = useState(null);
+    const [activeItem, setActiveItem] = useState(null);
 
-	return (
+    return (
         <>
-            {categories.map((category) => (
-                <dl className="px-2 text-sm leading-8 my-4">
-						<a className='text-lg font-bold' href="#">
-							<button
-								className="float-right mr-2 mt-3 block h-[9px] w-[9px]"
-								style={{
-									backgroundImage: `url("https://static.wconcept.co.kr/web/images/common/spr-common.png")`,
-									backgroundPositionX: '0',
-									backgroundPositionY: '-60px',
-									backgroundRepeat: 'no-repeat',
-								}}
-								></button>
-								{renderItem({name: category.title})}
-						</a>
-                    {category.items.map((item) => renderItem(item))}
-                </dl>
+            {categories.map((category, index) => (
+                <div key={index} className="px-2 text-sm leading-8 my-4">
+                    <a className="text-lg font-bold" href="#">
+                        <button
+                            className="float-right mr-2 mt-3 block h-[9px] w-[9px]"
+                            style={{
+                                backgroundImage:
+                                    `url("https://static.wconcept.co.kr/web/images/common/spr-common.png")`,
+                                backgroundPositionX: "0",
+                                backgroundPositionY: "-60px",
+                                backgroundRepeat: "no-repeat",
+                            }}
+                        ></button>
+                        <dl 
+                            key={`${index}-title`} 
+                            onClick={() => setActiveItem(category.title)} 
+                            className={activeItem === category.title ? "item-active" : ""}
+                        >
+                            <dt className="sr-only" aria-label="제목"></dt>
+                            <button>{category.title}</button>
+                        </dl>
+                    </a>
+
+                    {category.items.map((item, id) => (
+                        <dl 
+                            key={`${index}-${id}`} 
+                            onClick={() => setActiveItem(item)} 
+                            className={activeItem === item ? "item-active" : ""}
+                        >
+	                        <dt className="sr-only" aria-label="부제목"></dt>
+	                        <button>{item}</button>
+	                    </dl>
+	                ))}
+                </div>
             ))}
         </>
     );
-
-	function renderItem(item) {
-		let name;
-		
-		if (typeof item === "string") {
-			name = item;
-		} else if (typeof item === "object") {
-			name = item.name;
-	    }
-
-	    const isActive = activeItem === name;
-	    const className = isActive ? 'item-active' : '';
-
-	    return (
-	        <dd key={name} onClick={() => setActiveItem(name)} className={className}>
-	            <a href="#">{name}</a>
-	        </dd>
-	    );
-    }
 }
 
 export default ProductCategoryItem;
