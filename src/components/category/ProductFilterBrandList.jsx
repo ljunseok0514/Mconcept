cb8fd57ca273857cbc9558176ea3e55d4c995bc6
lagom60 <lagom6061@gmail.com>
2023-09-23 22:49:37 +0900

    🎨 [Styling] filter hover 수정 #148

--- src/components/category/ProductFilterBrandList.jsx
+++ src/components/category/ProductFilterBrandList.jsx
@@ -1,7 +1,7 @@
 import {forwardRef, useState} from 'react';
 import {PrimaryButton, SecondaryButton} from '@/components/category/ProductFilterButton';
-import {ProductFilterBrand, ProductFilterPrice, ProductFilterBenefit, ProductFilterColor, ProductFilterDiscount} from '@/components/category/ProductFilter';
-import { Link } from 'react-router-dom';
+import {ProductFilterBrand} from '@/components/category/ProductFilter';
+import {Link} from 'react-router-dom';
 
 /**
  *  ProductFilterList component
@@ -13,31 +13,29 @@ function ProductFilterList({brands, onFilter, onAssign, onReset}, ref) {
 	return (
 		<>
 			<div className="flex justify-between border-y-[1px] border-l-grey-200 bg-[#fbfbfb] px-6 py-4 text-lg font-semibold">
-				<h4 className="filter-button">
-					FILTER
-				</h4>
+				<h4 className="filter-button">FILTER</h4>
 				<ul className="filter-nav flex gap-24">
 					<Link to="/categoryBrand">
 						<button type="button" onClick={() => setActiveFilter('BRAND')} style={{color: activeFilter === 'BRAND' ? '#fa5500' : 'initial'}}>
 							BRAND
 						</button>
 					</Link>
-
+					{/* 
 					<li>
 						<button type="button" onClick={() => setActiveFilter('BENEFIT')} style={{color: activeFilter === 'BENEFIT' ? '#fa5500' : 'initial'}}>
 							BENEFIT
 						</button>
 					</li>
-					<Link to="/categoryColor">
+					<li>
 						<button type="button" onClick={() => setActiveFilter('COLOR')} style={{color: activeFilter === 'COLOR' ? '#fa5500' : 'initial'}}>
 							COLOR
 						</button>
-					</Link>
+					</li>
 					<li>
 						<button type="button" onClick={() => setActiveFilter('DISCOUNT')} style={{color: activeFilter === 'DISCOUNT' ? '#fa5500' : 'initial'}}>
 							DISCOUNT
 						</button>
-					</li>
+					</li> */}
 				</ul>
 			</div>
 
@@ -64,11 +62,10 @@ function ProductFilterList({brands, onFilter, onAssign, onReset}, ref) {
 export default forwardRef(ProductFilterList);
 
 {
-	/* <li key={benefit} className="flex pl-4">
-								{activeFilter === 'BENEFIT' && <ProductFilterBenefit brandName={benefit} onFilter={onFilter} />}
-							</li>
-
-							<li key={discount} className="flex pl-4">
-								{activeFilter === 'DISCOUNT' && <ProductFilterDiscount brandName={discount} onFilter={onFilter} />}
-							</li> */
+/* <li key={benefit} className="flex pl-4">
+{activeFilter === 'BENEFIT' && <ProductFilterBenefit brandName={benefit} onFilter={onFilter} />}
+</li>
+<li key={discount} className="flex pl-4">
+{activeFilter === 'DISCOUNT' && <ProductFilterDiscount brandName={discount} onFilter={onFilter} />}
+</li> */
 }
