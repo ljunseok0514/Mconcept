876eef98b420fc344f1182da0039dcbce5ed639a
lagom60 <lagom6061@gmail.com>
2023-09-24 09:17:55 +0900

    🎨 [Styling] footer mt-[90px] 추가

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,7 +5,7 @@
 function Footer() {
 	return (
 		<>
-			<footer className="mb-6">
+			<footer className="mb-6 mt-[90px]">
 				<div className="flex justify-center bg-grey-500 text-base font-normal text-white">
 					<div className="mt-18 max-w-[1920px] leading-[58px]">
 						<ul className="mx-auto grid w-[1240px] grid-cols-8 text-center">
@@ -59,10 +59,10 @@ function Footer() {
 							<dt>상호명 : ㈜더블유컨셉코리아 ㅣ 대표자 : 이은철 ㅣ 주소 : 서울특별시 강남구 테헤란로 231, EAST동 20층(역삼동, 센터필드)</dt>
 							<dt>
 								사업자등록번호 : 211-88-19183 ㅣ 통신판매업신고 : 제2009호-서울강남-00847호
-								<a className="ml-1 border border-black px-2 py-[1px] text-sm font-medium" href="/" target="_blank">
+								<a className="mx-1 border border-black px-2 py-[1px] text-sm font-medium" href="/" target="_blank">
 									사업자정보확인
 								</a>
-								│ 개인정보보호책임자 : 허선희 │ 호스팅서비스 : ㈜ 더블유컨셉코리아
+								개인정보보호책임자 : 허선희 │ 호스팅서비스 : ㈜ 더블유컨셉코리아
 							</dt>
 							<dt>COPYRIGHT ⓒ ㈜더블유컨셉코리아 ALL RIGHTS RESERVED</dt>
 						</dl>
