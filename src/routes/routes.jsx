4c813b7202a81d1babb2df25bcb0ad5ff4cf6128
lagom60 <lagom6061@gmail.com>
2023-09-25 06:29:42 +0900

    🐛 [Fix] category 링크 경로 수정

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import {lazy} from 'react';
 const Mainpage = lazy(() => import('@/pages/Mainpage'));
 const RootLayout = lazy(() => import('@/layout/RootLayout'));
 const CategoryBrand = lazy(() => import('@/pages/CategoryBrand'));
+const CategoryColor = lazy(() => import('@/pages/CategoryColor'));
 const CategoryOuter = lazy(() => import('@/pages/CategoryOuter'));
 const CategoryJacket = lazy(() => import('@/pages/CategoryJacket'));
 const CategoryJumper = lazy(() => import('@/pages/CategoryJumper'));
@@ -26,13 +27,13 @@ const router = createBrowserRouter(
 			<Route path="products/:id" element={<Details />} />
 			<Route path="categoryBrand" element={<CategoryBrand />} />
 			<Route path="categoryBrand/outer" element={<CategoryOuter />} />
-			<Route path="jacket" element={<CategoryJacket />} />
-			<Route path="jumper" element={<CategoryJumper />} />
-			<Route path="padding" element={<CategoryPadding />} />
-			<Route path="coat" element={<CategoryCoat />} />
-			<Route path="pants" element={<CategoryPants />} />
-			<Route path="tshirts" element={<CategoryTshirts />} />
-			<Route path="neat" element={<CategoryNeat />} />
+			<Route path="categoryBrand/jacket" element={<CategoryJacket />} />
+			<Route path="categoryBrand/jumper" element={<CategoryJumper />} />
+			<Route path="categoryBrand/padding" element={<CategoryPadding />} />
+			<Route path="categoryBrand/coat" element={<CategoryCoat />} />
+			<Route path="categoryBrand/pants" element={<CategoryPants />} />
+			<Route path="categoryBrand/tshirts" element={<CategoryTshirts />} />
+			<Route path="categoryBrand/neat" element={<CategoryNeat />} />
 		</Route>,
 	),
 );
